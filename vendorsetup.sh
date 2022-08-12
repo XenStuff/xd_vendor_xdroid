7d58eefa901006769a40189b0c56cacc1fffbbe3
Mayur <flynryder427@gmail.com>
2022-08-12 22:35:39 +0530

    K

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
+rm -rf vendor/xdroidui vendor/google/gms
+git clone https://gitlab.com/Project-Awaken/android_vendor_gms vendor/gms
+git clone https://github.com/XenStuff/xd_vendor_xdroidui vendor/xdroidui
+
 export AOSP_REVISION=$(grep "default revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
 
 # Welcome Msg
@@ -5,3 +9,7 @@ echo "=========================================="
 echo " xd. xdroidOS Build Environment "
 echo "=========================================="
 echo "AOSP Revision: $AOSP_REVISION"
+export KBUILDBUILDUSER=xenxynon 
+export KBUILDBUILDHOST=xenxynon 
+export BUILD_USERNAME=xenxynon
+export BUILD_HOSTNAME=xenxynon
