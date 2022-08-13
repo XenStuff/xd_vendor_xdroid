b95c8cfeb04f737acdfbe9d6a691a8afc429bc79
Mayur <flynryder427@gmail.com>
2022-08-13 06:45:55 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 rm -rf vendor/xdroidui vendor/google/gms vendor/gms
-git clone https://gitlab.com/Project-Awaken/android_vendor_gms vendor/gms
+git clone https://gitlab.com/Project-Awaken/android_vendor_gms vendor/gms --depth=1
 git clone https://github.com/XenStuff/xd_vendor_xdroidui vendor/xdroidui
 
 export AOSP_REVISION=$(grep "default revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
