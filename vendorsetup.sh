45778fe0921397252e770d26b9aeaefb5d299c43
Mayur <flynryder427@gmail.com>
2022-08-15 13:54:19 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
-rm -rf vendor && repo sync --force-sync > p
+mv ~/roms/RiceDroid-twelve/out ~/roms/xdroid*/out*
+make clean -j$(nproc --all)
+rm -rf vendor/google vendor/gms && repo sync --force-sync > p
 rm p
 export AOSP_REVISION=$(grep "default revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
 
